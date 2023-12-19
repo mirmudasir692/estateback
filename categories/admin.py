7cbb839108301966bb3463ec53bf2d3e523664df
mirmudasir692 <mirmudasir692@gmail.com>
2023-12-19 17:57:20 +0530

    add app

---
+++ categories/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Discount, Category
+
+"""
+show the categories to the admin panel and 
+discount model
+
+"""
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "image")
---
+++ categories/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Discount(models.Model):
+    event = models.CharField(max_length=100)
+    start_on = models.DateTimeField(auto_now_add=True)
+    end_on = models.DateTimeField(auto_now_add=False)
+    percentage_discount = models.IntegerField()
+
+    def __str__(self):
+        return self.event
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=300)
+    image = models.ImageField(upload_to="categories", null=True)
+
+
